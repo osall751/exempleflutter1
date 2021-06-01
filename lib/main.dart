c6fa696094cb9b08c5f8e743c2422b8aa2c19fc3
Ousmane Sall <osall@univ-thies.sn>
2021-06-01 18:12:32 +0000

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:projetmastegl/page1.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Page1(),
+    home: Page2(),
   ));
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Exemple 2"),
+      ),
+      body: Container(
+        child: Container(
+          color: Colors.green,
+          child: ListView(
+            // mainAxisSize: MainAxisSize.max,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Text1"),
+              Text("Text2"),
+              Text("Text3"),
+              Container(
+                color: Colors.amber,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Text4"),
+                    Text("Text5"),
+                    Text("Text6"),
+                  ],
+                ),
+              ),
+              Image.asset("images/img2.jpg"),
+              Image.asset("images/img3.jpg"),
+              Image.asset("images/img4.jpg")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
